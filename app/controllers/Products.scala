2979f3f4161b713b8cc6a81b076db07b86c116d1
jameshoare <jhoare0@gmail.com>
2014-01-25 09:52:45 +0000

    bit of caching

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -86,23 +86,26 @@ object Products extends Controller {
     // Encoded the address in order to remove the spaces from the address (spaces will be replaced by '+')
     //@purpose There should be no spaces in the parameter values for a GET request
     val addressEncoded = URLEncoder.encode(address, "UTF-8");
-    val jsonContainingLatitudeAndLongitude = WS.url("http://maps.googleapis.com/maps/api/geocode/json?address=" + addressEncoded + "&sensor=true").get().map {
-      response => (response.json \\ "location")
+
+    Cache.getOrElse("geocode", 10) {
+
+      val jsonContainingLatitudeAndLongitude = WS.url("http://maps.googleapis.com/maps/api/geocode/json?address=" + addressEncoded + "&sensor=true").get().map {
+        response => (response.json \\ "location")
+      }
+
+      // Wait until the future completes (Specified the timeout above)
+
+      val result = Await.result(jsonContainingLatitudeAndLongitude, timeout.duration).asInstanceOf[List[JsObject]]
+
+      //Fetch the values for Latitude & Longitude from the result of future
+      val latitude = (result(0) \\ "lat")(0).toString.toDouble
+      val longitude = (result(0) \\ "lng")(0).toString.toDouble
+      Option(latitude, longitude)
     }
-
-    // Wait until the future completes (Specified the timeout above)
-
-    val result = Await.result(jsonContainingLatitudeAndLongitude, timeout.duration).asInstanceOf[List[JsObject]]
-
-    //Fetch the values for Latitude & Longitude from the result of future
-    val latitude = (result(0) \\ "lat")(0).toString.toDouble
-    val longitude = (result(0) \\ "lng")(0).toString.toDouble
-    Option(latitude, longitude)
   }
 
 
   def cached() = Action.async {
-
 
 
     val results = 3
